e64c588da6fcc7e1e0e0dbe92b5c3d7abed0cb87
Ariq Syahalam <70618885+ariqsyahalam@users.noreply.github.com>
2024-04-26 22:13:40 +0700

    Initial Commit

---
+++ LegacyLensLite/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LegacyLensLite
+//
+//  Created by Reyhan Ariq Syahalam on 26/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LegacyLensLite/LegacyLensLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegacyLensLiteApp.swift
+//  LegacyLensLite
+//
+//  Created by Reyhan Ariq Syahalam on 26/04/24.
+//
+
+import SwiftUI
+
+@main
+struct LegacyLensLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
